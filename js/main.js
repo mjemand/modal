052d3e8b19329abd89cf05152f8e2877255a99e2
mjemand <mechle.tamas@gmail.com>
2020-11-19 01:02:17 +0100

    1st version

---
+++ js/main.js
@@ -0,0 +1,20 @@
+
+var modal = document.getElementById("theModal");
+
+var btn = document.getElementById("openBtn");
+
+var span = document.getElementsByClassName("closeX")[0];
+
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
